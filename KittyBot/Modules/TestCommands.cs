8fa3c6ef0ea7f86ad86f3417c895d3f936d67b0f
Evelyn Serra <143884194+EveMeows@users.noreply.github.com>
2025-02-13 19:51:53 +0200

    Add basic bot functionality, refactor file paths.

---
+++ KittyBot/Modules/TestCommands.cs
@@ -0,0 +1,18 @@
+﻿using DSharpPlus;
+using DSharpPlus.Entities;
+using DSharpPlus.SlashCommands;
+
+namespace KittyBot.Modules;
+
+public class TestCommands : ApplicationCommandModule
+{
+	[SlashCommand("ping", "Pong!")]
+	public async Task PingCommand(InteractionContext ctx)
+	{
+		await ctx.CreateResponseAsync(
+			InteractionResponseType.ChannelMessageWithSource,
+			new DiscordInteractionResponseBuilder()
+				.WithContent("Pong!")
+		);
+	}
+}
